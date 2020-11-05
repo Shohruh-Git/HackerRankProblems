2beb3faeae57e33d6cdf7becaaa5f70c39ecf36a
Shokhrukhmirzo Gulomov <73752036+Shohruh-Git@users.noreply.github.com>
2020-11-05 22:26:41 +0500

    O(n^2)

---
+++ Linked-List/Find Merge point of two linked lists.cpp
@@ -0,0 +1,16 @@
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode *temp = head1;
+    while(head2){
+        head1 = temp;
+        while(head1)
+        {
+            if(head1==head2)
+            {
+                return head1->data;
+            }
+            head1=head1->next; 
+        }
+        head2 = head2->next;
+    }
+    return 0;
+}
